a156a0275dc265c40edfdb4bd59d13f9fc873008
Ryan <mathewsthomasryan@gmail.com>
2023-11-02 08:42:08 -0500

    move function down

--- js.js
+++ js.js
@@ -1,8 +1,8 @@
+const ul = document.createElement('ul');
+const input = document.createElement('input');
+const butn = document.createElement('button');
+
 function btnClick()
 {
     
 }
-
-const ul = document.createElement('ul');
-const input = document.createElement('input');
-const butn = document.createElement('button');
